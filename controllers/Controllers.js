144066efc933e31c4c8fa1cd6246916893024373
travis-john <travis.wl.john@gmail.com>
2020-04-01 21:14:56 -0400

    adding controllers

---
+++ controllers/Controllers.js
@@ -0,0 +1,61 @@
+const axios = require('axios');
+const db = require('../models');
+
+module.exports = {
+  findAll: function(req, res) {
+    const { query: params } = req;
+    axios
+      .get("https://www.googleapis.com/books/v1/volumes", {
+        params
+      })
+      .then(results =>
+        results.data.items.filter(
+          result =>
+            result.volumeInfo.title &&
+            result.volumeInfo.infoLink &&
+            result.volumeInfo.authors &&
+            result.volumeInfo.description &&
+            result.volumeInfo.imageLinks &&
+            result.volumeInfo.imageLinks.thumbnail
+        )
+      )
+      .then(apiBooks =>
+        db.Book.find().then(dbBooks =>
+          apiBooks.filter(apiBook =>
+            dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
+          )
+        )
+      )
+      .then(books => res.json(books))
+      .catch(err => res.status(422).json(err));
+  }
+};
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Book.find(req.query)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Book.findById(req.params.id)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Book.create(req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book.findById(req.params.id)
+      .then(dbBook => dbBook.remove())
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  }
+};
