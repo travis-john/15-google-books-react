0ce4c33d76d62b819554bfa61ec1c3ccdf9060f6
travis-john <travis.wl.john@gmail.com>
2020-04-04 12:28:00 -0400

    converted footer to Reactstrap

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faGithub  }from '@fortawesome/free-brands-svg-icons'
+
 
 function Footer() {
   return (
     <footer>
       <hr />
       <p className="pull-right">
-        <i className="fab fa-github" /> Built using React.js and MongoDB
+        <FontAwesomeIcon className='fab' icon={faGithub} /> Built using React.js and MongoDB
       </p>
     </footer>
   );
